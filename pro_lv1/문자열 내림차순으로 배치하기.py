f6a321ba30d19f21bea16a58c054edbaf615135e
nayeonkiim <wkdrnsldk98@naver.com>
2020-08-03 22:27:01 +0900

    프로그래머스 lv1

---
+++ pro_lv1/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,6 @@
+s = "Zbcdefg"
+
+def solution(s):
+    for i in range(len(s)-1):
+        for j in range(i+1,len(s)-i):
+            if j
---
+++ pro_lv1/문자열을 정수로 바꾸기.py
@@ -0,0 +1,7 @@
+def solution(s):
+    answer = 0
+    if s[0] == '+':
+        s = s[1:]
+    answer = int(s)
+    return answer
+        
