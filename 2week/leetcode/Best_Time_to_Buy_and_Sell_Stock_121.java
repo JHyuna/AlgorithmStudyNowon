824d47f594e0347023b413896f982f2a8a0b179c
yerang <koyr9588@gmail.com>
2020-08-13 16:32:01 +0900

    주식을 사고팔기 가장 좋은 시점

---
+++ 2week/leetcode/Best_Time_to_Buy_and_Sell_Stock_121.java
@@ -0,0 +1,33 @@
+package week2;
+
+public class Best_Time_to_Buy_and_Sell_Stock_121 {
+
+	public static void main(String[] args) {
+		int[] test = {2,1,2,1,0,1,2};
+		maxProfit(test);
+	}
+
+	public static int maxProfit(int[] prices) {
+		int max = -1;
+		int min = -1;
+		int answer = 0;
+		for (int price : prices) {
+			if(max < price && min != -1) {
+				max = price;
+			}
+			if(min == -1 || min > price) {
+				if(answer < max - min) {
+					answer = max - min;
+				}
+				min = price;
+				max = -1;
+			}
+		}
+		
+		if(answer < max - min) {
+			answer = max - min;
+		}
+
+		return answer;
+	}
+}
