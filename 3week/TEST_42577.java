293fcf40d964b194cad73bd0a4676d6075376f22
yearang <book7080@naver.com>
2020-08-23 07:36:35 +0900

    전화번호 목록

---
+++ 3week/TEST_42577.java
@@ -0,0 +1,23 @@
+public class TEST_42577 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book.length; j++) {
+                if(i==j)
+                    continue;
+                if(phone_book[i].length() <= phone_book[j].length()) {
+                    if(phone_book[i].equals(phone_book[j].substring(0,phone_book[i].length()))) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
