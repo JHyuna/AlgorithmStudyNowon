c457b1d25090c8ade503bcc66e21b2d6015e73d4
Heegun <khg878@gmail.com>
2020-09-01 18:51:12 +0900

    py_algorithm

--- week04/프린터.py
+++ week04/프린터.py
@@ -1,25 +1,28 @@
 """https://programmers.co.kr/learn/courses/30/lessons/42587"""
 
 def solution(priorities, location):
-    
+
+    if len(priorities) == 1:
+        return 1
+
     result = []
     answer = 0
     for index , prior in enumerate(priorities):
         result.append((prior,index))
 
     while True:
-        first = result.pop(0)
-
+        first = result[0]
         if max(result)[0] > first[0]:
+            result.pop(0)
             result.append(first)
         else:
+            result.pop(0)
             answer += 1
             if first[1] == location:
                 break
-    
-
-    
+        
     return answer
+
 
 print(solution([2, 1, 3, 2],2))
 
